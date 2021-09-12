0fd348d137c038c391961fdab5bf407d19b7d4ef
NilsRondhuis <nilsrondhuis@ukr.net>
2021-09-12 09:05:23 +0300

    Настроил локал стор

--- src/js/render-list/form.js
+++ src/js/render-list/form.js
@@ -5,39 +5,30 @@ const refs = {
     list: document.querySelector('.list-task'),
 };
 
-const LOCAL_STORAGE = 'task-info';
+const LOCAL_STORAGE = 'make-task';
 
 refs.form.addEventListener('submit', onSubmit);
 
-localStorage.setItem(LOCAL_STORAGE, JSON.stringify(serverLocalStorage.dataBase));
-const getString = localStorage.getItem(LOCAL_STORAGE);
-const parseString = JSON.parse(getString);
-const result = makeMarkup(parseString);
-appendMarkup(result);
+const createEl = text => `<li class="item"><p class="content-task">${text}</p></li>`;
+
+const storageCollection = JSON.parse(localStorage.getItem(LOCAL_STORAGE));
+const dataFromStorage = storageCollection ? storageCollection : [];
+const result = dataFromStorage.map(string => createEl(string));
+const markup = result.join('');
+refs.list.insertAdjacentHTML('afterbegin', markup);
+
+// localStorage.clear()
 
 function onSubmit(e) {
     e.preventDefault();
     const value = e.currentTarget.elements.query.value;
-    serverLocalStorage.dataBase.push(value);
-    
-    localStorage.setItem(LOCAL_STORAGE, JSON.stringify(serverLocalStorage.dataBase));
-    const getString = localStorage.getItem(LOCAL_STORAGE);
-    const parseString = JSON.parse(getString);
-    
-    const result = makeMarkup(parseString);
-    appendMarkup(result);
+    refs.list.insertAdjacentHTML('beforeend', createEl(value));
+
+    const array = JSON.parse(localStorage.getItem(LOCAL_STORAGE));
+    const arrayForStorage = array ? array : [];
+    arrayForStorage.push(value);
+    localStorage.setItem(LOCAL_STORAGE, JSON.stringify(arrayForStorage));
+
     e.currentTarget.reset();
 }
 
-
-function makeMarkup(elements) {
-    return elements.map(el => {
-        return `
-        <li class="item"><p class="content-task">${el}</p></li>
-        `;
-    })
-}
-
-function appendMarkup(el) {
-    refs.list.innerHTML = el.join('');
-}
